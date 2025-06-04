de8152a33fba43578f63ffff42b1960f8b208806
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2025-06-04 23:06:45 +0100

    clerk setup

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,36 @@
+"use client";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import { useUser } from "@clerk/nextjs";
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  const { user } = useUser();
+  return (
+    <div className="flex items-center justify-between p-5">
+      {user && (
+        <h1 className="text-2xl">
+          {user?.firstName}
+          {`s`} Space
+        </h1>
+      )}
+
+      {/* Breadcrums */}
+      <div>
+        <SignedOut>
+          <SignInButton></SignInButton>
+        </SignedOut>
+
+        <SignedIn>
+          <UserButton></UserButton>
+        </SignedIn>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
