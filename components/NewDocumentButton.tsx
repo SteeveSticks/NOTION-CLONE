fc210791641e6489058eef47784e321f7c9c44dc
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2025-06-05 19:40:40 +0100

    newDocumentButton

--- components/NewDocumentButton.tsx
+++ components/NewDocumentButton.tsx
@@ -1,10 +1,26 @@
+"use client";
+
 import React from "react";
 import { Button } from "./ui/button";
+import { useTransition } from "react";
+import { useRouter } from "next/router";
 
 const NewDocumentButton = () => {
+  const [isPeding, startTransition] = useTransition();
+  const router = useRouter();
+
+  const handleCreateNewDocument = () => {
+    startTransition(async () => {
+      const { docId } = await createNewDocument();
+      router.push(`/doc/${docId}`);
+    });
+  };
+
   return (
     <div>
-      <Button>New Document</Button>
+      <Button onClick={handleCreateNewDocument} disabled={!isPeding}>
+        {isPeding ? "Creating" : "New Document"}
+      </Button>
     </div>
   );
 };
