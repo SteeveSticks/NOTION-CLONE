000eafbf615dea38630b20749723ea064510b90f
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2025-06-07 00:21:04 +0100

    brought out two key owner | editor

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import NewDocumentButton from "./NewDocumentButton";
 import { MenuIcon } from "lucide-react";
 import { useCollection } from "react-firebase-hooks/firestore";
-
 import {
   Sheet,
   SheetContent,
@@ -13,11 +12,30 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { useUser } from "@clerk/nextjs";
-import { collectionGroup, query, where } from "firebase/firestore";
+import {
+  collectionGroup,
+  DocumentData,
+  query,
+  where,
+} from "firebase/firestore";
 import { db } from "@/firebase";
+
+interface RoomDocument extends DocumentData {
+  createdAt: string;
+  role: "owner" | "editor";
+  roomId: string;
+  userId: string;
+}
 
 const Sidebar = () => {
   const { user } = useUser();
+  const [groupedData, setGroupedData] = useState<{
+    owner: RoomDocument[];
+    editor: RoomDocument[];
+  }>({
+    owner: [],
+    editor: [],
+  });
 
   const [data, loading, error] = useCollection(
     user &&
@@ -27,7 +45,39 @@ const Sidebar = () => {
       )
   );
 
-  useEffect(() => {}, [data]);
+  useEffect(() => {
+    if (!data) return;
+
+    const grouped = data?.docs.reduce<{
+      owner: RoomDocument[];
+      editor: RoomDocument[];
+    }>(
+      (acc, curr) => {
+        // Extract room data and group by role
+        const roomData = curr.data() as RoomDocument;
+
+        if (roomData.role === "owner") {
+          acc.owner.push({
+            id: curr.id,
+            ...roomData,
+          });
+        } else {
+          acc.editor.push({
+            id: curr.id,
+            ...roomData,
+          });
+        }
+
+        return acc;
+      },
+      {
+        owner: [],
+        editor: [],
+      }
+    );
+
+    setGroupedData(grouped);
+  }, [data]);
 
   const menuOptions = (
     <>
