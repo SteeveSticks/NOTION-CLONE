ec2bd459530c1921ae5ba200dc0b20115a00b7f1
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2025-06-07 15:32:22 +0100

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import { Button } from "@/components/ui/button";
+import { ArrowLeftCircle } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>building a notion clone app</h1>
-
-      <Button>Click me</Button>
+    <main className="flex space-x-2 items-center animate-pulse">
+      <ArrowLeftCircle className="w-9 h-9" />
+      <h1 className="font-bold text-sm">
+        Get started with creating a New Document
+      </h1>
     </main>
   );
 }
