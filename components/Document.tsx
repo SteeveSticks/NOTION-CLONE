489a7fb732f4c702d44375ccd69bc60b1d670ee8
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2025-06-08 00:45:41 +0100

    page configuration document.tsx

---
+++ components/Document.tsx
@@ -0,0 +1,59 @@
+"use client";
+import React, { FormEvent, useEffect, useState, useTransition } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "@/firebase";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+
+const Document = ({ id }: { id: string }) => {
+  const [data, loading, error] = useDocumentData(doc(db, "documents", id));
+  const [input, setInput] = useState("");
+  const [isUpdating, startTransition] = useTransition();
+
+  useEffect(() => {
+    if (data) {
+      setInput(data.title);
+    }
+  }, [data]);
+
+  const updateTitle = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (input.trim()) {
+      startTransition(async () => {
+        await updateDoc(doc(db, "document", id), {
+          title: input,
+        });
+      });
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <form onSubmit={updateTitle}>
+          {/* update title.... */}
+          <Input value={input} onChange={(e) => setInput(e.target.value)} />
+
+          <Button disabled={isUpdating}>
+            {isUpdating ? "Updating..." : "Update"}
+          </Button>
+
+          {/* IF */}
+          {/* isOnwer && InviteUser, DeleteDocument */}
+        </form>
+      </div>
+
+      <div>
+        {/* Manage User */}
+
+        {/* Avatars */}
+      </div>
+
+      {/* Collaborative Editor */}
+    </div>
+  );
+};
+
+export default Document;
--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,7 @@ import React from "react";
 const Header = () => {
   const { user } = useUser();
   return (
-    <div className="flex items-center justify-between p-5">
+    <div className="flex items-center justify-between p-3">
       {user && (
         <h1 className="text-2xl">
           {user?.firstName}
