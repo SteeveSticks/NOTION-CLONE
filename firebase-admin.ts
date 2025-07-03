3c0f214494a33fbefbfd129f7343dc6d66c50f5a
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2025-07-03 05:54:48 +0100

    add vercel, gitignore, update next.jsconfig and add vercel package

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -1,6 +1,7 @@
 import { initializeApp, getApps, App, getApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const serviceKey = require("@/service_key.json");
 
 let app: App;
