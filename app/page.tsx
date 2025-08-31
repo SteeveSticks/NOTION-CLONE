47f8fad74dce2940ddec82d3a452b1138879c6b6
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2025-08-31 02:16:16 +0100

    Improve UI/UX

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,17 @@ import { ArrowLeftCircle } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className="flex space-x-2 items-center animate-pulse">
-      <ArrowLeftCircle className="w-9 h-9" />
-      <h1 className="font-bold text-sm">
-        Get started with creating a New Document
-      </h1>
+    <main className="flex flex-col">
+      <div className="flex space-x-2 items-center animate-pulse mb-4">
+        <ArrowLeftCircle className="w-9 h-9" />
+        <h1 className="font-bold text-sm">
+          Get started with creating a New Document
+        </h1>
+      </div>
 
-      {/* Implement a new image feature here */}
-      {/* Also an image with picture showing the action */}
+      <div>
+        <img src="/image/notionclone.png" alt="Notion Clone" />
+      </div>
     </main>
   );
 }
