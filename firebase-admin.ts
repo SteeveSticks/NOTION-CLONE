6f194a5770fdf7d35211d4b027807fcfbe62926a
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2025-08-31 02:00:27 +0100

    Implement fixing the firebase staging bug

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -1,8 +1,7 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
 import { initializeApp, getApps, App, getApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-const serviceKey = require("@/service_key.json");
+import { serviceKey } from "@/service_key.json";
 
 let app: App;
 
