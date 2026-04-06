0b954db9fdd801e59acc87194d6a1c8ad8845624
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2026-04-06 17:39:48 +0100

    Implemented the service key fix

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -1,13 +1,13 @@
 import { initializeApp, getApps, App, getApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-import { serviceKey } from "@/service_key.json";
+import serviceKey from "@/service_key.json";
 
 let app: App;
 
 if (getApps().length === 0) {
   app = initializeApp({
-    credential: cert(serviceKey),
+    credential: cert(serviceKey as any),
   });
 } else {
   app = getApp();
