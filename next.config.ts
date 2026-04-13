732c4753b80e13b1bea54eebb99ab1d91213aa6a
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2026-04-13 08:34:04 +0100

    mobile cookie error fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    "https://notionclone-alpha.vercel.app/",
+    "http://localhost:3000/",
+    "https://smell-trash-impulse.ngrok-free.dev/",
+  ],
   typescript: {
     ignoreBuildErrors: true,
   },
